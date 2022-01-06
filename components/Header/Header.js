b12a60ce4ca22b40770c5634a99ca93fa8da76fe
liaomingjie <2986415715@qq.com>
2022-01-06 17:03:56 +0800

    demo

---
+++ components/Header/Header.js
@@ -0,0 +1,32 @@
+// components/Header/Header.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    // 声明接收属性
+    properties: {
+      title:{
+        type:String,
+        default:'标题'
+      },
+      content:{
+        type:String,
+        default:'内容'
+      }
+    },
+  
+    /**
+     * 组件的初始数据
+     */
+    data: {
+  
+    },
+  
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+  
+    }
+  })
+  
